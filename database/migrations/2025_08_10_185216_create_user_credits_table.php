d5f8bb7b21cd67dc8b07de18e98b4c93552fc56f
imkrad <164296287+imkrad@users.noreply.github.com>
2025-08-11 06:03:04 +0800

    j

---
+++ database/migrations/2025_08_10_185216_create_user_credits_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_credits', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->decimal('balance',12,2); 
+            $table->year('year');
+            $table->tinyInteger('leave_id')->unsigned()->index();
+            $table->foreign('leave_id')->references('id')->on('list_leaves')->onDelete('cascade');
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unique(['employee_id', 'leave_type_id', 'year']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_credits');
+    }
+};
